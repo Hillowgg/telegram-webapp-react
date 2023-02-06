32738c352b398a65c2aea9288ccaedc6ca5d9a2f
hillow <andrew.hillow@gmail.com>
2023-02-06 22:24:59 +0300

    added weekParity switcher

--- src/modules/DayChooser.js
+++ src/modules/DayChooser.js
@@ -1,34 +1,61 @@
 import React from 'react';
 
-// import { MainButton, useShowPopup } from '@vkruglikov/react-telegram-web-app';
-import {Radio} from 'antd';
+import {ConfigProvider, Radio, Switch} from 'antd';
 
-const divStyles = {
-    position: 'fixed',
-    bottom: 0,
-    display: 'grid',
-    align: 'center',
-    width: '100%',
-    height: '5vh'
-}
 
-export default function DayChooser(data) {
+export default function DayChooser(props) {
 
-    const onChange = (e) => {
-        data.setDay(e.target.value)
+    const onWeekDayChange = (e) => {
+        props.setDay(e.target.value)
+    }
+    const weekParityToggler = () => {
+        props.setWeekParity(props.weekParity === 'odd' ? 'even' : 'odd')
+    }
+
+    const mainDivStyles = {
+        position: 'fixed',
+        bottom: 0,
+        display: 'grid',
+        gridTemplateColumns: '2fr 12fr',
+        align: 'center',
+        width: '100%',
+        height: 'var(--daychoosing-height)',
+    }
+
+    const radioGroupStyles = {
+        padding: '0',
+        margin: '0',
+        color: 'orange'
+    }
+
+
+    const switchStyles = {
+        backgroundColor: 'orange',
+        fontFamily: 'serif'
+    }
+
+    const theme = {
+        token: {
+            colorPrimary: 'orange',
+        }
     }
 
     return (
-        <div style={divStyles}>
-            <Radio.Group onChange={onChange} defaultValue={0}>
-                <Radio.Button value={0}>Пн</Radio.Button>
-                <Radio.Button value={1}>Вт</Radio.Button>
-                <Radio.Button value={2}>Ср</Radio.Button>
-                <Radio.Button value={3}>Чт</Radio.Button>
-                <Radio.Button value={4}>Пт</Radio.Button>
-                <Radio.Button value={5}>Сб</Radio.Button>
-                <Radio.Button value={6}>Вс</Radio.Button>
-            </Radio.Group>
+        <div style={mainDivStyles}>
+
+            <Switch style={switchStyles} checkedChildren="II" unCheckedChildren="I" onChange={weekParityToggler}
+            />
+            <ConfigProvider theme={theme}>
+                <Radio.Group style={radioGroupStyles} onChange={onWeekDayChange} defaultValue={0}>
+                    <Radio.Button value={0}>Пн</Radio.Button>
+                    <Radio.Button value={1}>Вт</Radio.Button>
+                    <Radio.Button value={2}>Ср</Radio.Button>
+                    <Radio.Button value={3}>Чт</Radio.Button>
+                    <Radio.Button value={4}>Пт</Radio.Button>
+                    <Radio.Button value={5}>Сб</Radio.Button>
+                    <Radio.Button value={6}>Вс</Radio.Button>
+                </Radio.Group>
+            </ConfigProvider>
         </div>
     )
 }
