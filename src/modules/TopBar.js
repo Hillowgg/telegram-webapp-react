ba39c86341d66258768d57fd6422241b01bd30ad
hillow <andrew.hillow@gmail.com>
2023-02-06 22:25:26 +0300

    adaprive data parsing and fonts

--- src/modules/TopBar.js
+++ src/modules/TopBar.js
@@ -1,22 +1,35 @@
 import React from 'react';
 
-export default function TopBar() {
+
+function parseData(data, styles) {
+    return data.map((user, i) => {
+        return (<div style={{...styles, gridColumn: i + 1}}>{user.name}</div>)
+    })
+}
+
+export default function TopBar({data}) {
     const styles = {
-        backgroundColor: 'darkblue',
-        color: 'white',
-        height: '5vh',
+        display: 'grid',
+        // gridTemplateColumns: '1fr 1fr',
+        gridAutoColumns: '1fr',
+        gridGap: '0.5vh',
+        height: 'var(--topbar-height)',
         width: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
         position: 'fixed',
         zIndex: 1,
     }
+    const divStyles = {
+        backgroundColor: 'rgba(255,165,0, 0.5)',
+        color: 'black',
+        borderRadius: '5px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: '5vmin',
+    }
     return (
         <header style={styles} className="topbar">
-            <nav>
-                <h1>Расписание</h1>
-            </nav>
+            {parseData(data, divStyles)}
         </header>
     )
 }
